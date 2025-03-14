07a0a9b3aaec01e79ba77e183f43184ab6dbb31e
CybrHawk <104633356+CybrHawk@users.noreply.github.com>
2025-03-14 23:50:31 +0100

    Level1 & caracterbody : modifications sprite, gravité et résolution projet 1080p

--- Scenes/character_pink_2d.gd
+++ Scenes/character_pink_2d.gd
@@ -1,18 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 350.0
+const JUMP_VELOCITY = -800.0
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sprite_2d.animation = "jumping"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -20,6 +27,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 13)
 
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft 
