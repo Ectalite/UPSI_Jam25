38f1a010f58fe505ae711e707f94799b88a31a94
Xavier Hueber <xavier.hueber@tutanota.de>
2025-03-15 21:44:50 +0100

    Scene1: Plaques de pression et scene de test

---
+++ Scenes/button/button.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var sprite = $Sprite2D
+
+signal button_pressed
+signal button_released
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	sprite.texture = load("res://Scenes/button/plaquebas.png")
+	button_pressed.emit()
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	sprite.texture = load("res://Scenes/button/plaquehaut.png")
+	button_released.emit()
+
+func _ready() -> void:
+	sprite.texture = load("res://Scenes/button/plaquehaut.png")
---
+++ testlevel/test_level.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _on_button_button_pressed() -> void:
+	print("Pressed")
+
+
+func _on_button_button_released() -> void:
+	print("Released")
