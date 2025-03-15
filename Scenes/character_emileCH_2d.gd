884501972ad491b0a590faf18e66351c41a1d05c
CybrHawk <104633356+CybrHawk@users.noreply.github.com>
2025-03-15 09:25:43 +0100

    Ajout Second Joueur, modif noms : Hans et Emile
    Ajout sprite animé pour second joueur, controles séparés pour P1 et P2

---
+++ Scenes/character_emileCH_2d.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 350.0
+const JUMP_VELOCITY = -800.0
+@onready var sprite_emile_2d: AnimatedSprite2D = $SpriteEMILE2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_emile_2d.animation = "running"
+	else:
+		sprite_emile_2d.animation = "default"
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jumpP1") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		sprite_emile_2d.animation = "jumping"
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("leftP1", "rightP1")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 13)
+
+	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_emile_2d.flip_h = isLeft 
