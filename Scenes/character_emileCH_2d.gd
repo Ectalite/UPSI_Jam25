e6d0d3672d506162f16f113ad4e47b765773d593
CybrHawk <104633356+CybrHawk@users.noreply.github.com>
2025-03-15 16:54:33 +0100

    Changement controles, swap coté clavier Emile et Hans
    Emile cote gauche
    Hans coté droit

--- Scenes/character_emileCH_2d.gd
+++ Scenes/character_emileCH_2d.gd
@@ -16,13 +16,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jumpP1") and is_on_floor():
+	if Input.is_action_just_pressed("emile_saut") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		sprite_emile_2d.animation = "jumping"
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("leftP1", "rightP1")
+	var direction := Input.get_axis("emile_gauche", "emile_droite")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
