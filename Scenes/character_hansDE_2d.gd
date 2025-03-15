10c46c3a7ce5d35b3dc8bbe2174c57f133ec2f5e
CybrHawk <104633356+CybrHawk@users.noreply.github.com>
2025-03-15 15:11:54 +0100

    Ajout Sprite Emiles en fichiers

--- Scenes/character_hansDE_2d.gd
+++ Scenes/character_hansDE_2d.gd
@@ -28,7 +28,7 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, 13)
+		velocity.x = move_toward(velocity.x, 0, 15)
 
 	move_and_slide()
 	
