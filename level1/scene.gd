2ef8f7255a74e6a7e928fce93f1ab1b14cb50294
Xavier Hueber <xavier.hueber@tutanota.de>
2025-03-15 23:50:06 +0100

    Level1: working waterfall

--- level1/scene.gd
+++ level1/scene.gd
@@ -8,14 +8,33 @@ func _on_emileDoor_triggered():
 func _on_hansDoor_triggered():
 	hansDoor = true
 
+var waterfallTriggered = false
+@onready var waterfall = $Waterfall as TileMapLayer
+
+func _ready():
+	waterfall.visible = false
+	waterfall.set_process(false)
+
 func _process(delta):
 	if hansDoor and emileDoor:
 		#Change scene
 		SceneTransitioner.transition_to("res://level1/scene.tscn")
 
 func _on_waterfall_body_entered(body: Node2D) -> void:
-	print("Hello World waterfall")
-	var waterfall = $Layer1 as TileMapLayer
-	waterfall.visible = 0
-	waterfall.set_process(false)
+	if !waterfallTriggered:
+		waterfallTriggered = true
+		var sound = $Waterfall/WaterfallSound as AudioStreamPlayer
+		sound.play()
+
+func _on_button_waterfall_button_pressed() -> void:
+	waterfall.visible = true
+	waterfall.set_process(true)
 	
+func _on_button_waterfall_button_released() -> void:
+	waterfall.visible = false
+	waterfall.set_process(false)
+
+func _on_waterfall_sound_finished() -> void:
+	#Unlock doors
+	var doorEmile = $DoorEmile as Node2D
+	var doorHans = $DoorHans as Node2D
