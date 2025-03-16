6e576f567f092c775d0bc50f51393f08579719f6
CybrHawk <104633356+CybrHawk@users.noreply.github.com>
2025-03-16 10:04:32 +0100

    Changement musique pour loop

---
+++ level2/scene.gd
@@ -0,0 +1,47 @@
+extends Node
+
+var emileDoor = false
+var hansDoor = false
+
+func _on_emileDoor_triggered():
+	emileDoor = true
+func _on_hansDoor_triggered():
+	hansDoor = true
+
+var waterfallTriggered = false
+@onready var waterfall = $Waterfall as Node2D
+@onready var sound = $Waterfall/WaterfallSound as AudioStreamPlayer
+
+func _ready():
+	waterfall.visible = false
+	waterfall.set_process(false)
+	$Waterfall/WaterfallCollision.monitoring = false
+
+func _process(delta):
+	if hansDoor and emileDoor:
+		#Change scene
+		#SceneTransitioner.transition_to("res://level1/scene.tscn")
+		print("The end")
+
+func _on_button_waterfall_button_pressed() -> void:
+	waterfall.visible = true
+	waterfall.set_process(true)
+	$Waterfall/WaterfallCollision.monitoring = true
+	
+func _on_button_waterfall_button_released() -> void:
+	waterfall.visible = false
+	waterfall.set_process(false)
+	$Waterfall/WaterfallCollision.monitoring = false
+
+func _on_waterfall_sound_finished() -> void:
+	#Unlock doors
+	var doorEmile = $DoorEmile as Node2D
+	var doorHans = $DoorHans as Node2D
+	doorEmile.unlock()
+	doorHans.unlock()
+
+func _on_waterfall_waterfall_enterred() -> void:
+	print("Hello waterfall")
+	if !waterfallTriggered:
+		waterfallTriggered = true
+		sound.play()
