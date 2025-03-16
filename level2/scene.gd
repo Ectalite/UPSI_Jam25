6c8e29f6b771bc682b4a868c19b3a4c4a6cf4c4e
Xavier Hueber <xavier.hueber@tutanota.de>
2025-03-16 11:34:07 +0100

    Scene2: Add door and buttons

--- level2/scene.gd
+++ level2/scene.gd
@@ -3,6 +3,9 @@ extends Node
 var emileDoor = false
 var hansDoor = false
 
+var milieu1 = false
+var milieu2 = false
+
 func _on_emileDoor_triggered():
 	emileDoor = true
 func _on_hansDoor_triggered():
@@ -12,6 +15,25 @@ func _ready():
 	pass
 
 func _process(delta):
+	if milieu1 or milieu2:
+		$Milieu.enabled = false
+		$Milieu.visible = false
+	else:
+		$Milieu.enabled = true
+		$Milieu.visible = true
+			
 	if hansDoor and emileDoor:
 		#Change scene
 		SceneTransitioner.transition_to("res://credits/Credits.tscn")
+		
+func _on_ouverture_milieu_button_pressed() -> void:
+	milieu1 = true
+
+func _on_ouverture_milieu_button_released() -> void:
+	milieu1 = false
+
+func _on_ouverture_milieu_2_button_pressed() -> void:
+	milieu2 = true
+
+func _on_ouverture_milieu_2_button_released() -> void:
+	milieu2 = false
